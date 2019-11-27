a845cbd14b7fc283f6eb8f8672b181ff11997020
fvega <fer.vega@newsan.com.ar>
2019-11-27 13:06:37 -0300

    RestServer -usuario

--- server/server.js
+++ server/server.js
@@ -1,49 +1,28 @@
 require('./config/config');
-const express = require('express')
+const express = require('express');
 const app = express();
-const bodyParser = require('body-parser')
+
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-app.get('/usuario', function(req, res) {
-    //para enviar json cambiamos .send por .json
-    //res.send('Hello World')
-    res.json('get Usuario');
-});
 
-app.post('/usuario', function(req, res) {
-    let body = req.body;
+app.use(require('./routes/usuario'));
 
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es obligatorio'
-        });
-    } else {
 
-        res.json({
-            persona: body
-        });
-    }
+mongoose.connect(process.env.URLDB, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true },
+    (err, res) => {
+        if (err) throw err;
 
-});
-
-app.put('/usuario/:id', function(req, res) {
-    let id = req.params.id;
-    res.json({
-        id
+        console.log('Base de datos ONLINE');
     });
-});
 
 
-app.delete('/usuario', function(req, res) {
-
-    res.json('delete Usuario');
-});
 
 app.listen(process.env.PORT, () => {
     console.log("Escuchando Puerto: ", process.env.PORT);
