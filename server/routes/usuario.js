78c34c30905b543c27f5e84e6d304109e27f3caa
fvega <fer.vega@newsan.com.ar>
2019-11-28 13:53:08 -0300

    Login normal seccion 10

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
 const Usuario = require('../models/usuario');
+const { verificaToken, verificaAdminRole } = require('../middlewares/autenticacion');
 const bcrypt = require('bcrypt');
 const _ = require('underscore');
 
-app.get('/usuario', function(req, res) {
+app.get('/usuario', verificaToken, (req, res) => {
 
 
     let desde = req.query.desde || 0;
@@ -38,7 +39,7 @@ app.get('/usuario', function(req, res) {
         })
 });
 
-app.post('/usuario', function(req, res) {
+app.post('/usuario', [verificaToken, verificaAdminRole], (req, res) => {
     let body = req.body;
 
     let usuario = new Usuario({
@@ -66,7 +67,7 @@ app.post('/usuario', function(req, res) {
     });
 });
 
-app.put('/usuario/:id', function(req, res) {
+app.put('/usuario/:id', [verificaToken, verificaAdminRole], (req, res) => {
     let id = req.params.id;
     let body = _.pick(req.body, ['nombre', 'email', 'img', 'role', 'estado']);
 
@@ -88,7 +89,7 @@ app.put('/usuario/:id', function(req, res) {
 });
 
 
-app.delete('/usuario/:id', function(req, res) {
+app.delete('/usuario/:id', [verificaToken, verificaAdminRole], (req, res) => {
 
     let id = req.params.id;
 
--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 require('./config/config');
+
 const express = require('express');
 const app = express();
 
 const mongoose = require('mongoose');
+
 const bodyParser = require('body-parser');
 
 // parse application/x-www-form-urlencoded
@@ -11,9 +13,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
-
-app.use(require('./routes/usuario'));
-
+//Configuración Global de rutas
+app.use(require('./routes/index'));
 
 mongoose.connect(process.env.URLDB, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true },
     (err, res) => {
@@ -21,7 +22,6 @@ mongoose.connect(process.env.URLDB, { useNewUrlParser: true, useCreateIndex: tru
 
         console.log('Base de datos ONLINE');
     });
-
 
 
 app.listen(process.env.PORT, () => {
